2cb749731bab3201ef81c0c95b9ab4d62a7c2758
Miles <NaE>
2021-05-22 20:05:02 +0200

    Copied from MS repo

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,26 +1,31 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBar from '@/components/header-bar.vue';
+import NavBar from '@/components/nav-bar.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {};
+  },
   components: {
-    HelloWorld
-  }
-}
+    HeaderBar,
+    NavBar,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<template>
+  <div id="app">
+    <HeaderBar />
+    <div class="section columns">
+      <NavBar />
+      <main class="column">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import 'styles.scss';
 </style>
